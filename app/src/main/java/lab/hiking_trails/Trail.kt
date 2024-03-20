0411b7d12eb7087a8e78a0ad5e9a11a5ecd725cb
michal-kowal <kowalewski.michal777@gmail.com>
2024-03-20 14:10:37 +0100

    add trail class

---
+++ app/src/main/java/lab/hiking_trails/Trail.kt
@@ -0,0 +1,22 @@
+package lab.hiking_trails
+
+class Trail(id: Int, name: String, length: Float, description: String, localization: String,
+            imageId: Int, color: String) {
+    var id: Int
+    var name: String
+    var length: Float
+    var description: String
+    var localization: String
+    var color: String
+    var imageId: Int
+
+    init {
+        this.id = id
+        this.name = name
+        this.length = length
+        this.description = description
+        this.localization = localization
+        this.imageId = imageId
+        this.color = color
+    }
+}
