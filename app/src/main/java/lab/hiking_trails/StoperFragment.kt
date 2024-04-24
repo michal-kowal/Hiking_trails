4ff946dc6776d0015c345b96b527d4b6fa9d1cb1
michal-kowal <kowalewski.michal777@gmail.com>
2024-04-24 16:52:38 +0200

    add progress

---
+++ app/src/main/java/lab/hiking_trails/StoperFragment.kt
@@ -0,0 +1,100 @@
+package lab.hiking_trails
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import kotlin.properties.Delegates
+
+class StoperFragment : Fragment(), View.OnClickListener {
+    private var seconds = 0
+    private var running = false
+    private var wasRunning = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(savedInstanceState!=null){
+            seconds = savedInstanceState.getInt("seconds")
+            running = savedInstanceState.getBoolean("running")
+            wasRunning = savedInstanceState.getBoolean("wasRunning")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val layout = inflater.inflate(R.layout.fragment_stoper, container, false)
+        runStoper(layout)
+        val startButton = layout.findViewById<Button>(R.id.start_button)
+        startButton.setOnClickListener(this)
+        val stopButton = layout.findViewById<Button>(R.id.stop_button)
+        stopButton.setOnClickListener(this)
+        val resetButton = layout.findViewById<Button>(R.id.reset_button)
+        resetButton.setOnClickListener(this)
+        return layout
+    }
+
+    override fun onPause(){
+        super.onPause()
+        wasRunning = running
+        running = false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(wasRunning){
+            running=true
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("seconds", seconds)
+        outState.putBoolean("running", running)
+        outState.putBoolean("wasRunning", wasRunning)
+    }
+
+    private fun onClickStart(){
+        running = true
+    }
+
+    private fun onClickStop(){
+        running = false
+    }
+
+    private fun onClickReset(){
+        running = false
+        seconds = 0
+    }
+
+    private fun runStoper(view: View){
+        val timeView = view.findViewById<TextView>(R.id.time_view)
+        val handler = Handler()
+        handler.post(object: Runnable {
+            override fun run(){
+                val hours = seconds / 3600
+                val minutes = (seconds % 3600) / 60
+                val secs = seconds % 60
+                val time = String.format("%d:%02d:%02d", hours, minutes, secs)
+                timeView.text = time
+                if(running){
+                    seconds += 1
+                }
+                handler.postDelayed(this, 1000)
+            }
+        })
+    }
+
+    override fun onClick(view: View) {
+        when(view.getId()){
+            R.id.start_button -> onClickStart()
+            R.id.stop_button -> onClickStop()
+            R.id.reset_button -> onClickReset()
+        }
+    }
+}
